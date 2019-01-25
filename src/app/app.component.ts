825ec092c3e2a1ce6327ec1f1b9331407aa1c035
Paige Williams and Sheila Stephen <paige@>
2019-01-24 16:48:53 -0800

    add employee component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,26 +8,5 @@ import { Keg } from './models/keg.model';
 })
 export class AppComponent {
   title = 'tap-room';
-  selectedKeg: Keg = null;
-
-  masterKegsList: Keg[] = [
-    new Keg('Beer1', 'Beer2', 5, 6),
-    new Keg('Beer3', 'Beer4', 6, 7),
-    new Keg('Beer5', 'Beer6', 8, 9)
-  ];
-
-  addKeg(newKeg: Keg){
-    this.masterKegsList.push(newKeg);
-    console.log(this.masterKegsList);
-  }
-
-  editKeg(clickedKeg){
-  this.selectedKeg = clickedKeg;
-}
-
-  finishedEditing(){
-  this.selectedKeg = null;
-  console.log(this.selectedKeg)
-  }
 
 }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Keg } from './app/models/keg.model';
+
 
 @Component({
   selector: 'app-employee',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeComponent implements OnInit {
 
-  constructor() { }
+  selectedKeg: Keg = null;
 
-  ngOnInit() {
+  masterKegsList: Keg[] = [
+    new Keg('Beer1', 'Beer2', 5, 6),
+    new Keg('Beer3', 'Beer4', 6, 7),
+    new Keg('Beer5', 'Beer6', 8, 9)
+  ];
+
+  addKeg(newKeg: Keg){
+    this.masterKegsList.push(newKeg);
+    console.log(this.masterKegsList);
+  }
+
+  editKeg(clickedKeg){
+  this.selectedKeg = clickedKeg;
+}
+
+  finishedEditing(){
+  this.selectedKeg = null;
+  console.log(this.selectedKeg)
   }
 
 }
